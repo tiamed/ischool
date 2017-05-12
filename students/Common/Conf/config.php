ba1fbf8412b610fcf53b9347133ddd7ba2d116ad
tiamed <tiamedue@gmail.com>
2017-05-12 22:35:20 +0800

    Revert "关闭调试模式"
    This reverts commit 087c09e1694bc89a80d26ffcd6927b2f045e8a51.

--- students/Common/Conf/config.php
+++ students/Common/Conf/config.php
@@ -13,7 +13,7 @@ return array(
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  '',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数    
-    'DB_DEBUG'  			=>  false, // 数据库调试模式 开启后可以记录SQL日志
+    'DB_DEBUG'  			=>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
     'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
 
--- students/Runtime/Data/_fields/yello.question.php
+++
@@ -1,1 +0,0 @@
-a:6:{i:0;s:2:"id";i:1;s:4:"kind";i:2;s:7:"content";i:3;s:7:"info_id";s:3:"_pk";s:2:"id";s:5:"_type";a:4:{s:2:"id";s:7:"int(10)";s:4:"kind";s:11:"varchar(10)";s:7:"content";s:12:"varchar(255)";s:7:"info_id";s:7:"int(10)";}}
